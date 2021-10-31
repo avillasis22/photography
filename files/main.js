97d44dc2591bcd37ed5eac228cf02c21e084382f
avillasis22 <82188240+avillasis22@users.noreply.github.com>
2021-10-30 22:18:43 -0400

    test

--- files/main.js
+++ files/main.js
@@ -17,3 +17,43 @@ sr.reveal('.imageTwo', {
     reset: true,
     viewFactor: 0.2,
 });
+
+sr.reveal('.imageThree', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.2,
+});
+
+sr.reveal('.imageFour', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.2,
+});
+
+sr.reveal('.imageFive', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.2,
+});
+
+sr.reveal('.imageSix', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.2,
+});
