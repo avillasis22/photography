30e912c17c3762d07f13ed4b733a4483fda872be
avillasis22 <82188240+avillasis22@users.noreply.github.com>
2021-10-30 22:16:58 -0400

    Update main.js

--- files/main.js
+++ files/main.js
@@ -7,3 +7,13 @@ sr.reveal('.imageOne, .bioDiv', {
     reset: true,
     viewFactor: 0.2,
 });
+
+sr.reveal('.imageTwo', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.2,
+});
