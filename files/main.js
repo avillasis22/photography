35fbe4ec8537794c24f6f5f661a285c703eb31b4
avillasis22 <82188240+avillasis22@users.noreply.github.com>
2021-10-30 21:05:00 -0400

    Photography Portfolio

---
+++ files/main.js
@@ -0,0 +1,10 @@
+
+sr.reveal('.image, .caption, .bioDiv', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.2,
+});
